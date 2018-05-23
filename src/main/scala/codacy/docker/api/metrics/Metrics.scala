397d44550e1a77f2c5a15bba72492b39da72cc5b
Bruno Ferreira <bruno.ferreira@codacy.com>
2018-05-23 12:32:06 +0100

    Removed fields that are not necessary.

--- src/main/scala/codacy/docker/api/metrics/Metrics.scala
+++ src/main/scala/codacy/docker/api/metrics/Metrics.scala
@@ -3,10 +3,8 @@ package codacy.docker.api.metrics
 
 case class FileMetrics(filename: String,
                        complexity: Option[Int] = None,
-                       buildtime: Option[Long] = None,
                        loc: Option[Int] = None,
                        cloc: Option[Long] = None,
-                       duplicatedLines: Option[Int] = None,
                        nrMethods: Option[Int] = None,
                        nrClasses: Option[Int] = None,
                        lineComplexities: Set[LineComplexity] = Set.empty)
