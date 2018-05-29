909cbbcf68a076e81c490a836dfaa8905045eff4
Pedro Amaral <pamaral@codacy.com>
2018-05-29 11:41:39 +0100

    removed unused DuplicationRequest case class

--- src/main/scala/codacy/docker/api/duplication/Duplication.scala
+++ src/main/scala/codacy/docker/api/duplication/Duplication.scala
@@ -1,11 +1,5 @@
 package codacy.docker.api.duplication
 
-//This was declared in the seed along with the other two case classes.
-//I can see it being used in the worker nor the seed, should I remove it?
-case class DuplicationRequest(directory: String, files: List[String])
-
 case class DuplicationCloneFile(filePath: String, startLine: Int, endLine: Int)
 
 case class DuplicationClone(cloneLines: String, nrTokens: Int, nrLines: Int, files: Seq[DuplicationCloneFile])
-
-
