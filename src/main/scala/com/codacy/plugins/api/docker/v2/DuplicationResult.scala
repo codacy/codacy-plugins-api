e377cacec20f4b616f4375d5a9c36caf403f998e
Rodrigo Fernandes <rtfrodrigo@gmail.com>
2018-08-08 11:25:36 +0100

    Separate metrics, duplication and issues

--- src/main/scala/com/codacy/plugins/api/docker/v2/DuplicationResult.scala
+++ src/main/scala/com/codacy/plugins/api/docker/v2/DuplicationResult.scala
@@ -1,18 +1,39 @@
 package com.codacy.plugins.api.docker.v2
 
-import com.codacy.plugins.api.docker.v2
-import com.codacy.plugins.api.{ErrorMessage, Source}
+import com.codacy.plugins.api.Source
 
-sealed trait DuplicationResult extends Result
+import scala.concurrent.duration.FiniteDuration
+
+sealed trait DuplicationResult
 
 object DuplicationResult {
 
+  sealed trait Duplication extends DuplicationResult
+
   case class CloneFile(filePath: String, startLine: Int, endLine: Int)
 
-  case class Clone(cloneLines: String, nrTokens: Int, nrLines: Int, files: Seq[CloneFile]) extends DuplicationResult
+  case class Clone(cloneLines: String, nrTokens: Int, nrLines: Int, files: Seq[CloneFile]) extends Duplication
 
-  case class Problem(message: ErrorMessage, file: Option[Source.File], reason: v2.Problem.Reason)
-      extends v2.Problem
-      with DuplicationResult
+  sealed trait Log extends DuplicationResult
+
+  case class Context(reason: Reason) extends Log
+  case class Advisory(reason: Reason, file: Option[Source.File]) extends Log
+
+  sealed trait Problem extends DuplicationResult
+
+  case class Failure(reason: Reason, files: Set[Source.File]) extends Problem
+
+  sealed trait Reason
+
+  object Reason {
+    case class MissingOptions(missingKeys: Set[String]) extends Reason
+
+    case class OptionProblem(optionName: String, badValue: String, supportedValues: Set[String])
+    case class InvalidOptions(options: Set[OptionProblem]) extends Reason
+
+    case class TimedOut(timeout: FiniteDuration) extends Reason
+
+    case class OtherReason(message: String, output: Option[String], stacktrace: Option[String]) extends Reason
+  }
 
 }
--- src/main/scala/com/codacy/plugins/api/docker/v2/IssueResult.scala
+++ src/main/scala/com/codacy/plugins/api/docker/v2/IssueResult.scala
@@ -1,12 +1,16 @@
 package com.codacy.plugins.api.docker.v2
 
-import com.codacy.plugins.api.docker.v2
+import com.codacy.plugins.api.Source
 import com.codacy.plugins.api.results.Pattern
-import com.codacy.plugins.api.{ErrorMessage, Source}
+import com.codacy.plugins.api.results.Pattern.{Category, Level}
 
-sealed trait IssueResult extends Result
+import scala.concurrent.duration.FiniteDuration
+
+sealed trait IssueResult
 
 object IssueResult {
+
+  sealed trait Issue extends IssueResult
 
   case class Lines(begin: Source.Line, end: Option[Source.Line])
 
@@ -20,10 +24,47 @@ object IssueResult {
     override def toString: String = value
   }
 
-  case class Issue(file: Source.File, message: Message, patternId: Pattern.Id, line: Source.Line) extends IssueResult
+  case class Occurrence(file: Source.File,
+                        message: Message,
+                        patternId: Pattern.Id,
+                        line: Source.Line,
+                        level: Option[Level.Value] = Option.empty[Level.Value],
+                        categories: Set[Category.Value] = Set.empty[Category.Value])
+      extends Issue
 
-  case class Problem(message: ErrorMessage, file: Option[Source.File], reason: v2.Problem.Reason)
-      extends v2.Problem
-      with IssueResult
+  sealed trait Log extends IssueResult
+
+  case class Context(reason: Reason) extends Log
+  case class Advisory(reason: Reason, file: Option[Source.File]) extends Log
+
+  sealed trait Problem extends IssueResult
+
+  case class Failure(reason: Reason, files: Set[Source.File]) extends Problem
+
+  sealed trait Reason
+
+  object Reason {
+    case class MissingConfiguration(supportedFilename: Set[String]) extends Reason
+
+    case class ParameterProblem(patternId: String,
+                                parameterName: String,
+                                badValue: String,
+                                supportedValues: Set[String])
+    case class InvalidConfiguration(unsupportedPatterns: Set[String], unsupportedValues: Set[ParameterProblem])
+        extends Reason
+
+    case class MissingOptions(missingKeys: Set[String]) extends Reason
+
+    case class OptionProblem(optionName: String, badValue: String, supportedValues: Set[String])
+    case class InvalidOptions(options: Set[OptionProblem]) extends Reason
+
+    case class TimedOut(timeout: FiniteDuration) extends Reason
+
+    case class MissingArtifacts(supported: Set[String]) extends Reason
+
+    case class InvalidArtifacts(paths: Set[String]) extends Reason
+
+    case class OtherReason(message: String, output: Option[String], stacktrace: Option[String]) extends Reason
+  }
 
 }
