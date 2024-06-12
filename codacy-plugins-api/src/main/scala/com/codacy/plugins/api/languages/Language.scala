cf5011b0d227e8a97266b724e47f5e8abecbeaf9
Stefan Vacareanu <stefan@codacy.com>
2024-06-12 13:26:06 +0300

    Signing

--- codacy-plugins-api/src/main/scala/com/codacy/plugins/api/languages/Language.scala
+++ codacy-plugins-api/src/main/scala/com/codacy/plugins/api/languages/Language.scala
@@ -275,7 +275,7 @@ object Languages {
   case object OCaml extends Language(extensions = Set(".ml", ".mli", ".mly", ".mll"))
 
   case object Solidity extends Language(extensions = Set(".sol"))
-// Generic language is used in Semgrep to map languages. Currently, it's used for PLSQL
+// Generic language is used in Semgrep to map languages.
   case object Generic
       extends Language(
         extensions = Set(".trg",
